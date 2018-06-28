602b1e4ce23126a3fc5a2a3c7fc80b2062aec418
stefano <sirfoga@protonmail.com>
2018-06-28 23:17:16 +0200

    add logo

--- app/src/it/raceup/yolo/ui/window/Main.java
+++ app/src/it/raceup/yolo/ui/window/Main.java
@@ -1,19 +1,47 @@
 package it.raceup.yolo.ui.window;
 
 import it.raceup.yolo.models.data.Raw;
+import it.raceup.yolo.ui.utils.AboutDialog;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame {
+    private static final String THIS_PACKAGE = "com.raceup.ed.bms.BmsGui";
+    private static final String ICON_PATH = "/res/images/logo.png";
+    private static final Image appIcon = Toolkit.getDefaultToolkit().getImage(
+            THIS_PACKAGE.getClass().getResource(ICON_PATH)
+    );
+
     public Main() {
         super("YOLO | Race Up Electric Division");
         setup();
+        open();
     }
 
-    public void setup() {
+    private void setup() {
+        setupLayout();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setIconImage(appIcon);  // set icon
+    }
+
+    private void setupLayout() {
+        getContentPane().setLayout(
+                new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS)
+        );  // add components horizontally
+        getRootPane().setBorder(
+                BorderFactory.createEmptyBorder(
+                        10, 10, 10, 10
+                )
+        );  // border
+        createMenuBar();
+    }
+
+    private void open() {
         pack();
         setSize(800, 400);
+        setLocation(0, 0);  // top left corner
         setVisible(true);
     }
 
@@ -23,4 +51,64 @@ public class Main extends JFrame {
 
     public void update(Raw data) {
     }
+
+    private JMenuBar createMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(createFileMenu());  // file
+        menuBar.add(createEditMenu());  // edit
+        menuBar.add(createHelpMenu());  // help/about
+        return menuBar;
+    }
+
+    private JMenu createFileMenu() {
+        JMenu menu = new JMenu("File");  // file menu
+
+        JMenuItem item = new JMenuItem("Exit");
+        item.addActionListener(e -> System.exit(0));
+        menu.add(item);
+
+        return menu;
+    }
+
+    private JMenu createEditMenu() {
+        JMenu menu = new JMenu("Edit");  // file menu
+
+        JMenuItem item = new JMenuItem("Send CAN message");
+        menu.add(item);
+
+        return menu;
+    }
+
+    private JMenu createHelpMenu() {
+        JMenu menu = new JMenu("Help");  // help menu
+
+        JMenuItem item = new JMenuItem("Help");  // help menu -> help
+        item.addActionListener(e -> showHelpDialogOrFail());
+        menu.add(item);
+
+        item = new JMenuItem("About");  // help menu -> about
+        item.addActionListener(e -> showAboutDialogOrFail());
+        menu.add(item);
+
+        return menu;
+    }
+
+    /**
+     * Show a dialog about the app
+     */
+    private void showAboutDialogOrFail() {
+        String content = "";  // todo
+        String title = "About this app";
+        new AboutDialog(this, content, title).setVisible(true);
+    }
+
+    /**
+     * Show a help dialog about the app
+     */
+    private void showHelpDialogOrFail() {
+        String content = "";  // todo
+        String title = "Help";
+        new AboutDialog(this, content, title).setVisible(true);
+    }
+
 }
--- app/src/it/raceup/yolo/ui/window/Motor.java
+++ app/src/it/raceup/yolo/ui/window/Motor.java
@@ -3,17 +3,28 @@ package it.raceup.yolo.ui.window;
 import it.raceup.yolo.models.data.Raw;
 
 import javax.swing.*;
+import java.awt.*;
 
 import static it.raceup.yolo.models.data.Base.DNF;
-import static it.raceup.yolo.models.data.Raw.FLAGS;
+import static it.raceup.yolo.models.data.Raw.*;
 
 public class Motor extends JFrame {
+    private JTable flags, sp, temperature;
+
     public Motor(final String title) {
         super(title);
         setup();
     }
 
-    public void setup() {
+    private void setup() {
+        setupTables();
+        setupLayout();
+
+        setSize(800, 400);
+        setVisible(true);
+    }
+
+    private void setupTables() {
         String[] columns = new String[]{
                 "Key", "Value"
         };  // headers for the table
@@ -24,20 +35,78 @@ public class Motor extends JFrame {
             data[row][0] = FLAGS[row];
             data[row][1] = DNF;
         }
+        flags = new JTable(data, columns);
+        flags.setEnabled(false);  // non-editable cells
 
-        // create table with data
-        JTable table = new JTable(data, columns);
+        data = new Object[SET_POINTS.length][columns.length];
+        for (int row = 0; row < data.length; row++) {
+            data[row][0] = SET_POINTS[row];
+            data[row][1] = DNF;
+        }
+        sp = new JTable(data, columns);
+        sp.setEnabled(false);  // non-editable cells
 
-        // add the table to the frame
-        add(new JScrollPane(table));
-
-
-        setSize(800, 400);
-        setVisible(true);
+        data = new Object[TEMPERATURES.length][columns.length];
+        for (int row = 0; row < data.length; row++) {
+            data[row][0] = TEMPERATURES[row];
+            data[row][1] = DNF;
+        }
+        temperature = new JTable(data, columns);
+        temperature.setEnabled(false);  // non-editable cells
     }
 
-    public JPanel getContentPanel() {
-        return null;
+    private void setupLayout() {
+        getContentPane().setLayout(
+                new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS)
+        );  // add components horizontally
+        getRootPane().setBorder(
+                BorderFactory.createEmptyBorder(
+                        10, 10, 10, 10
+                )
+        );  // border
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
+        panel.setAlignmentX(JScrollPane.RIGHT_ALIGNMENT);
+        panel.setBorder(
+                BorderFactory.createEmptyBorder(
+                        10, 10, 10, 10
+                )
+        );  // border
+
+        panel.add(new JPanel());
+        panel.add(Box.createRigidArea(new Dimension(10, 0)));
+        panel.add(new JPanel());
+
+        add(panel);
+        add(new JPanel());
+
+        /*JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.LINE_AXIS));
+
+        JPanel leftPanel = new JPanel();
+        leftPanel.add(flags);
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        rightPanel.add(sp);
+        rightPanel.add(temperature);
+
+        JPanel leftHalf = new JPanel() {
+            // Don't allow us to stretch vertically.
+            public Dimension getMaximumSize() {
+                Dimension pref = getPreferredSize();
+                return new Dimension(Integer.MAX_VALUE,
+                        pref.height);
+            }
+        };
+        leftHalf.setLayout(new BoxLayout(leftHalf, BoxLayout.PAGE_AXIS));
+        leftHalf.add(leftPanel);
+
+        mainPanel.add(leftHalf);
+        mainPanel.add(rightPanel);
+        add(mainPanel);*/
+
     }
 
     public void update(Raw data) {
