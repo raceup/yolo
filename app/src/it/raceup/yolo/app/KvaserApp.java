2f3e98acd9b3439b38ed4b595d4ae565fc64edc5
stefano <sirfoga@protonmail.com>
2018-09-04 17:06:44 +0200

    add version updater

--- app/src/it/raceup/yolo/app/KvaserApp.java
+++ app/src/it/raceup/yolo/app/KvaserApp.java
@@ -3,6 +3,7 @@ package it.raceup.yolo.app;
 import it.raceup.yolo.control.Hal;
 
 public abstract class KvaserApp extends YoloApp {
+    private static final String VERSION = "";
     protected Hal hal;
 
     public KvaserApp(String tag) {
