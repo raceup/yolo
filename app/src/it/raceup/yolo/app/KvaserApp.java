c674156439a4b539f14b7aa3eab63aeca9331587
stefano <sirfoga@protonmail.com>
2018-09-04 17:21:09 +0200

    log version

--- app/src/it/raceup/yolo/app/KvaserApp.java
+++ app/src/it/raceup/yolo/app/KvaserApp.java
@@ -2,12 +2,18 @@ package it.raceup.yolo.app;
 
 import it.raceup.yolo.control.Hal;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public abstract class KvaserApp extends YoloApp {
-    private static final String VERSION = "";
+    private static final String VERSION_FILE = "/res/strings/version.txt";
     protected Hal hal;
 
     public KvaserApp(String tag) {
         super(tag);
+
+        logVersion();
     }
 
     @Override
@@ -26,5 +32,20 @@ public abstract class KvaserApp extends YoloApp {
     @Override
     public void close() {
         hal.close();
+    }
+
+    private void logVersion() {
+        String version = getVersion();
+        log(version);
+    }
+
+    public String getVersion() {
+        try {
+            InputStream in = getClass().getResourceAsStream(VERSION_FILE);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            return reader.readLine();
+        } catch (Exception e) {
+            return "unknown";
+        }
     }
 }
