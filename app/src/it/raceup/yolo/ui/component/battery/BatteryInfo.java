b04546a98fa14f691b17123ce5f4c37fd611362f
sirfoga <sirfoga@protonmail.com>
2018-09-27 10:13:37 +0200

    add bms info

--- app/src/it/raceup/yolo/ui/component/battery/BatteryInfo.java
+++ app/src/it/raceup/yolo/ui/component/battery/BatteryInfo.java
@@ -2,5 +2,36 @@ package it.raceup.yolo.ui.component.battery;
 
 import javax.swing.*;
 
+import static it.raceup.yolo.models.data.Base.getAsString;
+
 public class BatteryInfo extends JPanel {
+    private static String VOLTAGE = "Total voltage: ";
+    private static String CURRENT = "Current: ";
+    private final JLabel voltageLabel = new JLabel(VOLTAGE);
+    private final JLabel currentLabel = new JLabel(CURRENT);
+
+    public BatteryInfo() {
+        setup();
+    }
+
+    private void setup() {
+        setupLayout();
+    }
+
+    private void setupLayout() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        add(voltageLabel);
+        add(currentLabel);
+    }
+
+    public void setVoltage(double value) {
+        String text = getAsString(value) + "V";
+        voltageLabel.setText(VOLTAGE + text);
+    }
+
+    public void setCurrent(double value) {
+        String text = getAsString(value) + "A";
+        voltageLabel.setText(CURRENT + text);
+    }
 }
