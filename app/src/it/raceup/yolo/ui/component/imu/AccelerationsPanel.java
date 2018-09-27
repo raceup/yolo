b49152ced1a70d7fa2d31998d5524a0cea5e5c6a
sirfoga <sirfoga@protonmail.com>
2018-09-27 17:15:32 +0200

    add accelerations panel

--- app/src/it/raceup/yolo/ui/component/imu/AccelerationsPanel.java
+++ app/src/it/raceup/yolo/ui/component/imu/AccelerationsPanel.java
@@ -5,26 +5,82 @@ import it.raceup.yolo.ui.component.PolarPlane2D;
 import javax.swing.*;
 import java.awt.*;
 
+import static it.raceup.yolo.models.data.Base.getAsString;
+
 public class AccelerationsPanel extends JPanel {
-    private final PolarPlane2D accelerationPlane;
+    private static final String X_LABEL = "X: ";
+    private static final String Y_LABEL = "Y: ";
+    private static final String TOT_LABEL = "Magnitude: ";
+    private final PolarPlane2D plane;
+    private final JLabel xAccelerationLabel = new JLabel(X_LABEL);
+    private final JLabel yAccelerationLabel = new JLabel(Y_LABEL);
+    private final JLabel totAccelerationLabel = new JLabel(TOT_LABEL);
+    private double currentX = 0.0;
+    private double currentY = 0.0;
 
     public AccelerationsPanel() {
         super();
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));  // vertically
 
-        accelerationPlane = new PolarPlane2D(new Dimension(250, 245));
-        accelerationPlane.setAlignmentX(Component.CENTER_ALIGNMENT);
-        add(accelerationPlane);
-
+        plane = new PolarPlane2D(new Dimension(250, 250));
+        setup();
     }
 
-    /**
-     * Sets new value of acceleration to given axis
-     *
-     * @param value acceleration
-     * @param axis  acceleration axis
-     */
+    public static double getTotalMagnitude(double x, double y) {
+        double xSquared = x * x;
+        double ySquared = y * y;
+        double squared = xSquared + ySquared;
+        return Math.sqrt(squared);
+    }
+
+    private void setup() {
+        plane.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        setupLayout();
+    }
+
+    private void setupLayout() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));  // vertically
+
+        add(getTopPanel());
+        add(Box.createRigidArea(new Dimension(0, 10)));
+        add(plane);
+    }
+
+    private JPanel getTopPanel() {
+        JPanel panel = new JPanel();
+
+        // horizontally
+        panel.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+        panel.add(xAccelerationLabel);
+        panel.add(Box.createRigidArea(new Dimension(50, 0)));
+
+        panel.add(yAccelerationLabel);
+        panel.add(Box.createRigidArea(new Dimension(50, 0)));
+
+        panel.add(totAccelerationLabel);
+        panel.add(Box.createRigidArea(new Dimension(50, 0)));
+
+        return panel;
+    }
+
     public void setValue(double value, int axis) {
-        accelerationPlane.setValue(value, axis);
+        plane.setValue(value, axis);
+
+        if (axis == 0) {
+            currentX = value;
+            xAccelerationLabel.setText(X_LABEL + getAsString(value));
+        } else {
+            currentY = value;
+            yAccelerationLabel.setText(Y_LABEL + getAsString(value));
+        }
+
+        double totAcceleration = getTotalMagnitude(currentX, currentY);
+        totAccelerationLabel.setText(getAsString(totAcceleration));
+    }
+
+    public void setXYValue(double x, double y) {
+        plane.setValue(x, 0);
+        plane.setValue(y, 1);
     }
 }
